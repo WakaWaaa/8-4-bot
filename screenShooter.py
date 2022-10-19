920308c28faf3ad79714899de0131a9f6a0ef1ec
WakaWaaa <104302733+WakaWaaa@users.noreply.github.com>
2022-10-19 15:21:16 +0300

    Update screenShooter.py
    Updated the file. Currently sends the screenshot of the correct region on every message. Soon we'll add the neccasery logic to check the score of the game!

--- screenShooter.py
+++ screenShooter.py
@@ -1,22 +1,44 @@
-import discord
 import pyautogui
-import PIL
 import tkinter as tk
 
 root = tk.Tk()
-canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1 = tk.Canvas(root, width = 10, height = 10)
 canvas1.pack()
 
 scrheight = root.winfo_screenheight()
 scrwidth = root.winfo_screenwidth()
 
-path = "D:\very cool programing projects\Discord\8-4 bot\screenshots\screenshot.png"
+path = "your path here"
 
 def screenShot():
-    myScrnSht = pyautogui.screenshot(region = (0,0,scrwidth, scrheight/10))
-    myScrnSht.save(r'D:\very cool programing projects\Discord\8-4 bot\screenshots\screenshot.png')
-    root.destroy()
+    myScrnSht = pyautogui.screenshot(region = (0,0,scrwidth, scrheight/8))
+    myScrnSht.save(r'your path here')
 
-b = tk.Button(text = "Screenshot", command = screenShot)
-b.pack()
+screenShot()
+root.destroy()
+
+
+import discord
+
+TOKEN = 'your token here'
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    screenShot()
+    await message.channel.send(file=discord.File(r'your path here'))
+
+client.run(TOKEN)
+
 root.mainloop()
